b7a56d1e55d54d882ccc532872bb36a54d01006c
t0stiman <18124323+t0stiman@users.noreply.github.com>
2024-03-25 13:28:07 +0100

    Show reverser percent on steam engines and RNF on diesels

--- src/Patches/LocomotiveControlsUIAdapter_Patch.cs
+++ src/Patches/LocomotiveControlsUIAdapter_Patch.cs
@@ -1,21 +1,28 @@
 ﻿using HarmonyLib;
+using Model.Definition;
 using UI;
 using UnityEngine;
 
 namespace better_ui_mod.Patches;
 
 /// <summary>
-/// reverser
+/// reverser number instead of R N F on steam locomotives
 /// </summary>
 [HarmonyPatch(typeof(LocomotiveControlsUIAdapter))]
 [HarmonyPatch(nameof(LocomotiveControlsUIAdapter.ReverserText))]
 public class LocomotiveControlsUIAdapter_ReverserText_Patch
 {
-	private static bool Prefix(float abstractReverser, ref string __result)
+	private static bool Prefix(LocomotiveControlsUIAdapter __instance, float abstractReverser, ref string __result)
 	{
 		if (!Main.MySettings.ShowReverserPercentage)
 		{
 			return true; //execute original function
+		}
+		
+		// keep R N F on diesels
+		if (TrainController.Shared.SelectedLocomotive.Definition.Archetype == CarArchetype.LocomotiveDiesel)
+		{
+			return true;
 		}
 		
 		// abstractReverser is the value of the reverser. -1 is 100% reverse, 0 is neutral, 1 is 100% forward
