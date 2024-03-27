94a2fcd914b9aafd1cdd165f84f81ed98792f3e4
t0stiman <18124323+t0stiman@users.noreply.github.com>
2024-03-27 11:20:33 +0100

    Show the selected throttle notch instead of the throttle percentage

--- src/Patches/LocomotiveControlsUIAdapter_Patch.cs
+++ src/Patches/LocomotiveControlsUIAdapter_Patch.cs
@@ -1,5 +1,7 @@
 ﻿using HarmonyLib;
+using Model;
 using Model.Definition;
+using RollingStock;
 using UI;
 using UnityEngine;
 
@@ -16,19 +18,75 @@ public class LocomotiveControlsUIAdapter_ReverserText_Patch
 	{
 		if (!Main.MySettings.ShowReverserPercentage)
 		{
-			return true; //execute original function
+			return Stuff.EXECUTE_ORIGINAL;
 		}
 		
 		// keep R N F on diesels
 		if (TrainController.Shared.SelectedLocomotive.Definition.Archetype == CarArchetype.LocomotiveDiesel)
 		{
-			return true;
+			return Stuff.EXECUTE_ORIGINAL;
 		}
 		
 		// abstractReverser is the value of the reverser. -1 is 100% reverse, 0 is neutral, 1 is 100% forward
 		var reverserPercent = Mathf.RoundToInt(abstractReverser * 100);
 		__result = reverserPercent.ToString();
+
+		return Stuff.SKIP_ORIGINAL;
+	}
+}
+
+/// <summary>
+/// ShowThrottleNotchInsteadOfPercentage
+/// </summary>
+[HarmonyPatch(typeof(LocomotiveControlsUIAdapter))]
+[HarmonyPatch(nameof(LocomotiveControlsUIAdapter.Update))]
+public class LocomotiveControlsUIAdapter_Update_Patch
+{
+	private static bool Prefix(LocomotiveControlsUIAdapter __instance)
+	{
+		if (!Main.MySettings.ShowThrottleNotchInsteadOfPercentage)
+		{
+			return Stuff.EXECUTE_ORIGINAL;
+		}
+
+		var locomotive = LocomotiveControlsUIAdapter.Locomotive;
+		__instance.controls.gameObject.SetActive(locomotive != null);
+		if (locomotive == null)
+		{
+			return Stuff.SKIP_ORIGINAL;
+		}
+
+		__instance._updatingControls = true;
+		var locomotiveControl = locomotive.locomotiveControl;
+		__instance.throttleSlider.SetValueUnlessDragging(locomotiveControl.AbstractThrottle);
+		__instance.reverserSlider.SetValueUnlessDragging(locomotiveControl.AbstractReverser);
+		__instance.locomotiveBrakeSlider.SetValueUnlessDragging(locomotiveControl.LocomotiveBrakeSetting);
+		__instance.trainBrakeSlider.SetValueUnlessDragging(locomotiveControl.TrainBrakeSetting);
 		
-		return false;
+		// ====== changed: ======
+
+		var throttleNotches = locomotiveControl.ThrottleNotches == 0 ? 100 : locomotiveControl.ThrottleNotches;
+		var selectedNotch = locomotiveControl.AbstractThrottle * throttleNotches;
+		__instance.throttleSlider.handleText.SetText(Mathf.RoundToInt(selectedNotch).ToString());
+		
+		// ======================
+		
+		__instance.reverserSlider.handleText.text = LocomotiveControlsUIAdapter.ReverserText(locomotiveControl.AbstractReverser);
+		__instance.reverserSlider.wholeNumbers = locomotive.Archetype != CarArchetype.LocomotiveSteam;
+		__instance.simpleSlider.SetValueUnlessDragging(__instance.SimpleValue());
+		__instance.directionSlider.value = __instance.DirectionValue();
+		if (__instance.simpleSlider.value > 0.15000000596046448)
+		{
+			__instance.simpleSlider.handleText.SetText("{0}%", Mathf.RoundToInt(locomotiveControl.AbstractThrottle * 100f));
+		}
+		else
+		{
+			__instance.simpleSlider.handleText.text = __instance.simpleSlider.value >= -0.15000000596046448 ? "N" : "B";
+		}
+
+		__instance.directionSlider.handleText.text = __instance.directionSlider.value < 0.0 ? "R" : "F";
+		__instance._updatingControls = false;
+
+		return Stuff.SKIP_ORIGINAL;
 	}
 }
--- src/Settings.cs
+++ src/Settings.cs
@@ -6,11 +6,12 @@ namespace better_ui_mod
 	public class Settings : UnityModManager.ModSettings
 	{
 		public bool ShowReverserPercentage = true;
+		public bool ShowThrottleNotchInsteadOfPercentage = true;
 		public bool ShowComsumablesPercent = true;
 		public bool ShowSteamProfileButton = true;
 		public bool AlsoSearchLobbiesByName = true;
 		public bool HidePasswordProtectedLobbies = false;
-		public bool ShowMaxPlayers = true;
+		// public bool ShowMaxPlayers = true;
 
 		public void Setup(){}
 		
@@ -22,13 +23,20 @@ namespace better_ui_mod
 			AlsoSearchLobbiesByName = GUILayout.Toggle(AlsoSearchLobbiesByName, "Search by lobby name as well as reporting mark");
 			HidePasswordProtectedLobbies = GUILayout.Toggle(HidePasswordProtectedLobbies,
 				"Hide password-protected lobbies");
-			ShowMaxPlayers = GUILayout.Toggle(ShowMaxPlayers, "Show maximum amount of players");
+			// ShowMaxPlayers = GUILayout.Toggle(ShowMaxPlayers, "Show maximum amount of players"); //TODO
+			
+			GUILayout.Space(10);
+			GUILayout.Label("Controls");
+			GUILayout.Space(10);
+			
+			ShowReverserPercentage = GUILayout.Toggle(ShowReverserPercentage, "Show reverser percentage instead of R/N/F on steam locomotives");
+			ShowThrottleNotchInsteadOfPercentage = GUILayout.Toggle(ShowThrottleNotchInsteadOfPercentage,
+				"Show the selected throttle notch instead of the throttle percentage");
 			
 			GUILayout.Space(10);
 			GUILayout.Label("Misc.");
 			GUILayout.Space(10);
 			
-			ShowReverserPercentage = GUILayout.Toggle(ShowReverserPercentage, "Show reverser percentage instead of R/N/F");
 			ShowComsumablesPercent = GUILayout.Toggle(ShowComsumablesPercent, "Show fill percentage on cargo wagons, tenders, diesel locomotives, coaling towers and diesel fuel stands");
 			ShowSteamProfileButton = GUILayout.Toggle(ShowSteamProfileButton,
 				"Show a button in the 'Employees' menu that takes you to the steam profile of the selected player");
